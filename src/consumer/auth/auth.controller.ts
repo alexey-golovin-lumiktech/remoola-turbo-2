5e6a5efe63d721ac75b101f791f87dd68f13fc2d
Alexey Golovin <alexey.golovin@wbcs.ventures>
2023-04-04 19:38:38 +0300

    basic google oauth flow

--- src/consumer/auth/auth.controller.ts
+++ src/consumer/auth/auth.controller.ts
@@ -1,16 +1,33 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common'
+import { Body, Controller, Inject, Logger, Post } from '@nestjs/common'
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
 import { AccessToken, LoginBody } from '../../dtos'
 import { AuthService } from './auth.service'
+import { ConfigService } from '@nestjs/config'
+import { LoginTicket, OAuth2Client } from 'google-auth-library'
+import { GoogleProfile } from 'src/dtos/consumer/google-profile.dto'
 
 @ApiTags(`consumer`)
 @Controller(`consumer/auth`)
 export class AuthController {
-  constructor(@Inject(AuthService) private readonly service: AuthService) {}
+  logger = new Logger(AuthController.name)
+
+  constructor(@Inject(AuthService) private readonly service: AuthService, private readonly configService: ConfigService) {}
 
   @Post(`/login`)
   @ApiOkResponse({ type: AccessToken, status: 200 })
   login(@Body() body: LoginBody): Promise<AccessToken> {
     return this.service.login(body)
+  }
+
+  @Post(`/google-login`)
+  async googleLogin(@Body() { credential: idToken }: { credential: string }): Promise<any> {
+    const audience = this.configService.get<string>(`GOOGLE_CLIENT_ID`)
+    const secret = this.configService.get<string>(`GOOGLE_CLIENT_SECRET`)
+    const client = new OAuth2Client(audience, secret)
+    const verified: LoginTicket = await client.verifyIdToken({ idToken, audience })
+    const userID: string = verified.getUserId()
+    const profile = new GoogleProfile(userID, verified.getPayload())
+    this.logger.log({ from: `consumer/auth/google-login`, profile })
+    return `ok`
   }
 }
---
+++ src/dtos/consumer/google-profile.dto.ts
@@ -0,0 +1,46 @@
+import { Expose } from 'class-transformer'
+import { TokenPayload as ITokenPayload } from 'google-auth-library'
+
+export type ITokenPayloadPick = Pick<ITokenPayload, `email` | `email_verified` | `name` | `given_name` | `family_name` | `exp` | `picture`>
+
+export class TokenPayload implements ITokenPayloadPick {
+  @Expose()
+  public email?: string
+
+  @Expose()
+  public emailVerified?: boolean
+
+  @Expose()
+  public name?: string
+
+  @Expose()
+  public givenName?: string
+
+  @Expose()
+  public familyName?: string
+
+  @Expose()
+  public exp: number
+
+  @Expose()
+  public picture: string
+
+  constructor(payload: ITokenPayload) {
+    this.email = payload.email
+    this.emailVerified = payload.email_verified
+    this.name = payload.name
+    this.givenName = payload.given_name
+    this.familyName = payload.family_name
+    this.picture = payload.picture
+  }
+}
+
+export class GoogleProfile extends TokenPayload {
+  @Expose()
+  userID: string
+
+  constructor(userId: string, payload: ITokenPayload) {
+    super(payload)
+    this.userID = userId
+  }
+}
