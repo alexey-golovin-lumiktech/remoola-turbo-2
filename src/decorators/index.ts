efd96ba6fc37d1fb7b05a20df04142bddea4908e
Alexey Golovin <alexey.golovin@wbcs.ventures>
2023-06-26 10:57:00 +0300

    continue-preparing-remove-extra-decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,4 +1,2 @@
-export * from './authorization.decorator'
 export * from './public-endpoint.decorator'
 export * from './response-count-rows.decorator'
-export * from './user.decorator'
--- src/decorators/user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common'
-
-export const REQUEST_USER = Symbol(`user`)
-export const RequestUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest()
-  return request[REQUEST_USER]
-})
