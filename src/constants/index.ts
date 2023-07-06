9cbb8b660c57985f30fc8c77c9c25c1bb74fe2e9
Alexey Golovin <alexey.golovin@wbcs.ventures>
2023-07-06 09:06:12 +0300

    move-getEnvPath-to-utils

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,16 +1,4 @@
-import { existsSync } from 'fs'
-import { resolve } from 'path'
-
-export function getEnvPath(dest: string): string {
-  const env: string | undefined = process.env.NODE_ENV
-  const fallback: string = resolve(`${dest}/.env`)
-  const filename: string = env ? `.env.${env}` : `.env.development`
-  let filePath: string = resolve(`${dest}/${filename}`)
-
-  if (!existsSync(filePath)) filePath = fallback
-
-  return filePath
-}
+import { getEnvPath } from '../utils'
 
 export const INVALID_CREDENTIALS = `Invalid Credentials`
 export const ADMIN_NOT_FOUND = `Admin not found`
--- src/utils/provided-envs-checking.util.ts
+++ src/utils/provided-envs-checking.util.ts
@@ -1,6 +1,19 @@
 import * as dotenv from 'dotenv'
 import * as fs from 'fs'
-import { getEnvPath } from 'src/constants'
+import { existsSync } from 'fs'
+import { resolve } from 'path'
+
+// @NOTE: this fn should be defined here only
+export function getEnvPath(dest: string): string {
+  const env: string | undefined = process.env.NODE_ENV
+  const fallback: string = resolve(`${dest}/.env`)
+  const filename: string = env ? `.env.${env}` : `.env.development`
+  let filePath: string = resolve(`${dest}/${filename}`)
+
+  if (!existsSync(filePath)) filePath = fallback
+
+  return filePath
+}
 
 const customEnvironmentNames = [
   `PORT`,
