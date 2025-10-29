324c5249077a47dd078cc9bedb5b0bcc25b49d55
Aleksey Golovin <alexey.golovin@lumiktech.com>
2025-10-29 14:50:13 +0300

    vercel-cookie-logs

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -1,8 +1,14 @@
 // apps/admin/src/middleware.ts
 import { NextResponse, type NextRequest } from 'next/server';
 
+const logger = console;
+
 export function middleware(req: NextRequest) {
   const token = req.cookies.get(`access_token`)?.value;
+  logger.log(`\n************************************`);
+  logger.log(`token`, token);
+  logger.log(`req.nextUrl`, req.nextUrl);
+  logger.log(`************************************\n`);
   const { pathname } = req.nextUrl;
 
   if (!token && !pathname.startsWith(`/login`)) {
