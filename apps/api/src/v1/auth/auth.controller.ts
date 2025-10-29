f2816cd17e8b02a6f31b821f940e1ad62d4a9e84
Aleksey Golovin <alexey.golovin@lumiktech.com>
2025-10-29 14:44:09 +0300

    vercel-cookie-issue

--- apps/api/src/v1/auth/auth.controller.ts
+++ apps/api/src/v1/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
 
   private setAuthCookies(res: express.Response, access: string, refresh: string) {
     const isProd = parsedEnvs.NODE_ENV == `production`;
-    const domain = isProd ? parsedEnvs.COOKIE_DOMAIN : undefined; // ❌ don't set on 127.0.0.1(127.0.0.1)
+    const domain = isProd ? parsedEnvs.COOKIE_DOMAIN : undefined; // ❌ don't set on localhost(127.0.0.1)
     const sameSite = isProd ? (`none` as const) : (`lax` as const);
     const secure = isProd || parsedEnvs.COOKIE_SECURE == `true`;
 
@@ -24,14 +24,9 @@ export class AuthController {
       httpOnly: true,
       sameSite,
       secure,
-      domain: process.env.VERCEL ? `.vercel.app` : domain,
+      domain,
       path: `/`,
     };
-
-    console.log(`\n************************************`);
-    console.log(`common`, common);
-    console.log(`process.env.VERCEL`, process.env.VERCEL);
-    console.log(`************************************\n`);
 
     res.cookie(`access_token`, access, { ...common, maxAge: 1000 * 60 * 15 });
     res.cookie(`refresh_token`, refresh, { ...common, maxAge: 1000 * 60 * 60 * 24 * 7 });
