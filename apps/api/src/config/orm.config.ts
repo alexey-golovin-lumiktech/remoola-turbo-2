f36db191786b7aabfb65a1cc7e39f4e70ee1c346
Aleksey Golovin <alexey.golovin@lumiktech.com>
2025-10-29 11:38:47 +0300

    api-migrations-datasource

--- apps/api/src/config/orm.config.ts
+++ apps/api/src/config/orm.config.ts
@@ -5,21 +5,25 @@ import { parsedEnvs } from '@remoola/env';
 
 import { SnakeNamingStrategy } from './naming.strategy';
 
+export const config = {
+  type: `postgres` as const,
+  url: parsedEnvs.DATABASE_URL,
+  ssl: parsedEnvs.POSTGRES_SSL == `true` ? { rejectUnauthorized: false } : false,
+  autoLoadEntities: true,
+  entities: [__dirname + `/../**/*.entity.{ts,js}`],
+  migrations: [__dirname + `/../database/migrations/*.{ts,js}`],
+  synchronize: false,
+  logging: false,
+  extra: { options: `-c timezone=${parsedEnvs.POSTGRES_TIMEZONE}` },
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
 export const ormConfig: TypeOrmModuleAsyncOptions = {
-  useFactory: () => ({
-    type: `postgres`,
-    url: parsedEnvs.DATABASE_URL,
-    ssl: parsedEnvs.POSTGRES_SSL == `true` ? { rejectUnauthorized: false } : false,
-    autoLoadEntities: true,
-    entities: [__dirname + `/../**/*.entity.{ts,js}`],
-    migrations: [__dirname + `/../database/migrations/*.{ts,js}`],
-    synchronize: false,
-    logging: false,
-    extra: { options: `-c timezone=${parsedEnvs.POSTGRES_TIMEZONE}` },
-    namingStrategy: new SnakeNamingStrategy(),
-  }),
+  useFactory: () => config,
   dataSourceFactory: (options: DataSourceOptions) => {
     const dataSource = new DataSource(options);
     return dataSource.initialize();
   },
 };
+
+export default new DataSource(config);
