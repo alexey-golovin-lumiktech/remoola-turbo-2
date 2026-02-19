2d048e42b56a6b0a8e4f8e481cccf95835c2e1ba
Aleksey Golovin <alexey.golovin@lumiktech.com>
2026-02-19 17:16:04 +0300

    mi

--- packages/database-2/prisma/migrations/20260219140433_get_rid_unused/migration.sql
+++ packages/database-2/prisma/migrations/20260219140433_get_rid_unused/migration.sql
@@ -4,8 +4,3 @@
   - You are about to drop the `payment_request_expectation_date_archive` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE "payment_request_expectation_date_archive";
-
--- RenameIndex
-ALTER INDEX "idx_payment_method_billing_details_id" RENAME TO "payment_method_billing_details_id_idx";
