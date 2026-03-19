0848e779abd2b9e8408240fcc3604c856dd82846
Aleksey Golovin <alexey.golovin@lumiktech.com>
2026-03-19 09:29:01 +0300

    get rid unused @remoola/env

--- packages/env/.prettierrc.mjs
+++
@@ -1,3 +0,0 @@
-import config from '@remoola/eslint-config/prettier-base';
-
-export default config;
--- packages/env/eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-// @ts-ignore - eslint config package currently ships JS-only subpath exports.
-import { nestJsConfig } from "@remoola/eslint-config/nest-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nestJsConfig;
